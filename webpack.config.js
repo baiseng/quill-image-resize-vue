169828e3603a74726dbfa6414854460cb0ec9f30
swmonk <swmonk@qq.com>
2019-10-08 16:19:27 +0800

    fixed

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+var path = require('path');
+
+module.exports = {
+    entry: "./src/index.js",
+    output: {
+        path: __dirname,
+        library: 'ImageResize',
+        libraryTarget: 'umd',
+        filename: "image-resize-vue.min.js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                include: path.join(__dirname, 'src'),
+                exclude: /(node_modules|bower_components)/,
+                use: [{
+                    loader: 'babel-loader',
+                    options: {
+                        "presets": [["es2015", { "modules": false }]],
+                        "plugins": ["babel-plugin-transform-class-properties"]
+                    }
+                }]
+            },
+            {
+                test: /\.svg$/,
+                use: [{
+                    loader: 'raw-loader'
+                }]
+            }
+        ]
+    }
+};
